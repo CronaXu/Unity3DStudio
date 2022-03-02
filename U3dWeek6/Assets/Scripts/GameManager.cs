a230a261ae897937e047e62c31108172a312cb89
Crona Xu <cx640@nyu.edu>
2022-03-02 00:42:13 -0500

    Lighting project duplicate

---
+++ U3dWeek6/Assets/Scripts/GameManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    // This is private, so that we can show an error if its not set up yets
+    public List<GameObject> Apples = new List<GameObject>();
+    private int CurrentIndex = -1;
+
+    private static GameManager staticInstance;
+
+    public float NumberOfPoints = 0;
+
+    public GameObject door;
+    //private int timer = 100;
+
+    public AudioSource playerAudio;
+    public AudioClip doorOpenSound;
+
+
+    public static GameManager Instance
+    {
+        get
+        {
+            // If the static instance isn't set yet, throw an error
+            if (staticInstance is null)
+            {
+                Debug.LogError("Game Manager is NULL");
+            }
+
+            return staticInstance;
+        }
+    }
+
+    private void Awake()
+    {
+        // Set the static instance to this instance
+        staticInstance = this;
+        
+    }
+
+    public void AddPoints(float PointsToAdd)
+    {
+        Debug.Log("Points Added: " + PointsToAdd);
+        
+        NumberOfPoints += PointsToAdd;
+        Debug.Log("Current Score: " + NumberOfPoints);
+
+        if (NumberOfPoints == 49)
+        {
+            door.AddComponent<Rigidbody>();
+            door.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+            door.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX;
+            door.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ;
+
+            playerAudio.clip = doorOpenSound;
+            playerAudio.Play();
+
+        }
+    }
+
+    public void OnArchEnter(GameObject appleObject)
+    {
+        Debug.Log(Apples.IndexOf(appleObject));
+        CurrentIndex = Apples.IndexOf(appleObject);
+    }
+
+    void Update()
+    {
+        
+        //if (NumberOfPoints >= 49)
+        //{
+            
+        //    timer--;
+            
+        //}
+
+        //if (timer < 1)
+        //{
+        //    door.GetComponent<MeshCollider>().convex = true;
+        //}
+    }
+}
